db80730529d4c2f2fef4c96ed973dd9db760cefc
bavilona119-ship-it <bavilona119@gmail.com>
2025-11-07 15:39:44 +0300

    Update _layout.tsx

--- prodev-mobile-app-0x06/app/(home)/_layout.tsx
+++ prodev-mobile-app-0x06/app/(home)/_layout.tsx
@@ -1,1 +1,30 @@
+import { AntDesign, EvilIcons, Feather, FontAwesome, Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+
+const HomeRootLayout = () => {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "#34967C",
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => <AntDesign name="home" size={24} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: "Search",
+          headerShown: true,
+          tabBarIcon: ({ color }) => <Feather name="search" size={24} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="saved"
+        options={{
 
