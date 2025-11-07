34e586ae4f402ef6cfc1824dee1636c25450f11f
bavilona119-ship-it <bavilona119@gmail.com>
2025-11-07 14:58:31 +0300

    Create inbox.tsx

---
+++ prodev-mobile-app-0x05/app/(home)/inbox.tsx
@@ -0,0 +1,23 @@
+import { View, Text, StyleSheet } from "react-native";
+
+const Inbox = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Inbox Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "600",
+  },
+});
+
+export default Inbox;
